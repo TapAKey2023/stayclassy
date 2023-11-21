009a10b7d304e565b7cc3a5fd7b06445d4fba53c
Alex Young <zitayeung596@gmail.com>
2023-11-21 16:18:18 +0000

    validations on Booking Lesson models

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,9 @@
 class Lesson < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :price_per_hour, presence: true, numericality: { greater_than: 0 }
+  validates :duration, presence: true, numericality: { greater_than: 0 }
 end
