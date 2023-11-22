c35a439a08d9f2f35619b602f6236bb0265ba7fd
Alex Young <zitayeung596@gmail.com>
2023-11-22 14:11:56 +0000

    added pic url in lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -39,7 +39,7 @@ class LessonsController < ApplicationController
     @lesson = Lesson.new(lesson_params)
     @lesson.user = current_user
     if @lesson.save
-      redirect_to lesson_path(@lesson)
+      redirect_to lessons_path
     else
       render :new, status: :unprocessable_entity
     end
