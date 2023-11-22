b88705c3012a6105d2138ac4fa8610a8c1769051
Alessandra Zanelli <ali_zanelli@yahoo.co.uk>
2023-11-22 13:28:00 +0000

    ammended navbar links changed home route and some index styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,37 +34,37 @@
 puts "Creating user 1 and their lessons..."
 user_1 = User.new(email: "Visar@hotmail.com", password: "Visar123", name: "Visar")
 user_1.save!
-lesson_1 = Lesson.new(name: "Meditating", description:"Feel very zen. Learn mindfulness. Chill class for all!", user_id: user_1.id)
+lesson_1 = Lesson.new(name: "Meditating", description:"Feel very zen. Learn mindfulness. Chill class for all!", picture_url: "https://m.media-amazon.com/images/I/81Fjo7YQXFL._AC_SX522_.jpg", user_id: user_1.id)
 lesson_1.save!
-lesson_2 = Lesson.new(name: "Profiterole Tasting", description:"Taste some delicious cream puffs with chocolate, like a connoisseur", user_id: user_1.id)
+lesson_2 = Lesson.new(name: "Profiterole Tasting", description:"Taste some delicious cream puffs with chocolate, like a connoisseur", picture_url:"https://sugarspunrun.com/wp-content/uploads/2023/04/Profiteroles-6-of-9.jpg", user_id: user_1.id)
 lesson_2.save!
 
 puts "Creating user 2 and their lessons..."
 user_2 = User.new(email: "Jess@hotmail.com", password: "Jess123", name: "Jess")
 user_2.save
-lesson_3 = Lesson.new(name: "Cat Grooming 101", description:"Learn how to give your cat a proper spa day", user_id: user_2.id)
-lesson_3.save
-lesson_4 = Lesson.new(name: "Marie Antionette's Hairstyles", description:"She had some great wigs", user_id: user_2.id)
-lesson_4.save
+# lesson_3 = Lesson.new(name: "Cat Grooming 101", description:"Learn how to give your cat a proper spa day", user_id: user_2.id)
+# lesson_3.save
+# lesson_4 = Lesson.new(name: "Marie Antionette's Hairstyles", description:"She had some great wigs", user_id: user_2.id)
+# lesson_4.save
 
-puts "Creating user 3 and their lessons..."
-user_3 = User.new(email: "Alex@hotmail.com", password: "Alex123", name: "Alex")
-user_3.save
-lesson_5 = Lesson.new(name: "JavaScript for Gaming", description:"Make a enmdless runner game in JS", user_id: user_3.id)
-lesson_5.save
-lesson_6 = Lesson.new(name: "Dinosaurs: when to run", description:"Their vision is based on movement", user_id: user_3.id)
-lesson_6.save
+# puts "Creating user 3 and their lessons..."
+# user_3 = User.new(email: "Alex@hotmail.com", password: "Alex123", name: "Alex")
+# user_3.save
+# lesson_5 = Lesson.new(name: "JavaScript for Gaming", description:"Make a enmdless runner game in JS", user_id: user_3.id)
+# lesson_5.save
+# lesson_6 = Lesson.new(name: "Dinosaurs: when to run", description:"Their vision is based on movement", user_id: user_3.id)
+# lesson_6.save
 
-puts "Creating bookings..."
-booking_1 = Booking.new(user_id:user_1.id, lesson_id: 3, start_date: DateTime.new(2023,11,22,17) )
-booking_1.save!
-booking_2 = Booking.new(user_id:user_1.id, lesson_id: 5, start_date: DateTime.new(2023,11,22,17) )
-booking_2.save
-booking_3 = Booking.new(user_id: user_2.id, lesson_id: 1, start_date: DateTime.new(2023,11,22,17) )
-booking_3.save
-booking_4 = Booking.new(user_id: user_2.id, lesson_id: 5, start_date: DateTime.new(2023,11,22,17) )
-booking_4.save
-booking_5 = Booking.new(user_id: user_3.id, lesson_id: 2, start_date: DateTime.new(2023,11,22,17) )
-booking_5.save
-booking_6 = Booking.new(user_id: user_3.id, lesson_id: 4, start_date: DateTime.new(2023,11,22,17) )
-booking_6.save
+# puts "Creating bookings..."
+# booking_1 = Booking.new(user_id:user_1.id, lesson_id: 3, start_date: DateTime.new(2023,11,22,17) )
+# booking_1.save!
+# booking_2 = Booking.new(user_id:user_1.id, lesson_id: 5, start_date: DateTime.new(2023,11,22,17) )
+# booking_2.save
+# booking_3 = Booking.new(user_id: user_2.id, lesson_id: 1, start_date: DateTime.new(2023,11,22,17) )
+# booking_3.save
+# booking_4 = Booking.new(user_id: user_2.id, lesson_id: 5, start_date: DateTime.new(2023,11,22,17) )
+# booking_4.save
+# booking_5 = Booking.new(user_id: user_3.id, lesson_id: 2, start_date: DateTime.new(2023,11,22,17) )
+# booking_5.save
+# booking_6 = Booking.new(user_id: user_3.id, lesson_id: 4, start_date: DateTime.new(2023,11,22,17) )
+# booking_6.save
